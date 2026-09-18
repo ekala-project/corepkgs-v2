6f3b0bb4df7472505bb8d50a26f7924d3319b4ce
Jörg Thalheim <joerg@thalheim.io>
2026-09-19 01:02:41 +0200

    jsoncpp: build on msvc
    cmake with the set's cppdap insists on the set's jsoncpp too. Shared and
    static both want lib/jsoncpp.lib there, so no static one.

--- pkgs/js/jsoncpp/package.nix
+++ pkgs/js/jsoncpp/package.nix
@@ -1,19 +1,17 @@
 {
   package,
   buildPkgs,
+  platform,
 }:
 package {
   name = "jsoncpp";
-  # cmake sets MSVC only for cl-style drivers, the project keys windows specifics on it
-  platforms.abi = [
-    "gnu"
-    "apple"
-  ];
   uses = [ "cmake" ];
   cmake.tool = buildPkgs.cmake-bootstrap; # cmake links this
   cmake.defs = {
     JSONCPP_WITH_TESTS = false;
     JSONCPP_WITH_POST_BUILD_UNITTEST = false;
     BUILD_OBJECT_LIBS = false;
+    # import lib and static lib are both lib/jsoncpp.lib there
+    BUILD_STATIC_LIBS = platform.libc != "msvc";
   };
 }

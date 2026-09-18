a1b9f20d774e5c0934cb2522915a668406c90c8b
Jörg Thalheim <joerg@thalheim.io>
2026-09-18 08:00:25 +0200

    gawk: upstream fix for 5.4.1 built without MPFR
    Without mpfr_t in it the NODE value union is smaller than the code assumes and
    an unassigned array element compares unequal to "". libpng's checksym.awk
    then failed with "symbol 200 both exported as '0' and removed". Upstream
    commit bf85f8a, also carried by nixpkgs.

--- pkgs/ga/gawk/package.nix
+++ pkgs/ga/gawk/package.nix
@@ -6,7 +6,11 @@ package {
   uses = [ "autotools" ];
   bootstrapTools = true;
   # AWKPATH, AWKLIBPATH, locale and awklib helpers relative to the binary
-  patches = [ ./relocatable.patch ];
+  patches = [
+    ./relocatable.patch
+    # 5.4.1 without MPFR: unassigned array elements compare != "" (breaks libpng's checksym.awk)
+    ./upstream-node-struct-without-mpfr.patch
+  ];
   autotools.flags = [ "--disable-mpfr" ];
   tests.run = false; # locale-dependent, wants a full /usr/share/locale
   bin = [
